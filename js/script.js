1252c0be830736a325366656e5c5722505e8057c
singgahngopi <139740100+singgahngopi@users.noreply.github.com>
2023-07-25 15:37:50 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ window.addEventListener('DOMContentLoaded', () => {
   video.play();
 });
 
+// Play the video when it's ready and muted
+video.addEventListener('canplay', () => {
+  video.play();
+  video.muted = true;
+});
+
 // After the video ends, show the page content and hide the video
 video.addEventListener('ended', () => {
   videoContainer.style.opacity = 0;
@@ -56,34 +62,7 @@ var swiper = new Swiper(".review-slider", {
     },
 });
 
-let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
 
 
 
